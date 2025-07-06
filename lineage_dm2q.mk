bf27030669227235a566e6cf5791e2f0dc3b74f5
Mezaque Silver <mezaquegit@gmail.com>
2025-07-06 19:48:24 -0300

    dm2q: Update blobs from S9160ZHS6DYF1
    Change-Id: I00fc59768f35df69a219f7fc62a593e8b32483d0

--- lineage_dm2q.mk
+++ lineage_dm2q.mk
@@ -23,7 +23,7 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="dm2qzhx-user 15 AP3A.240905.015.A2 S9160ZHU6DYD9 release-keys" \
-    BuildFingerprint=samsung/dm2qzhx/dm2q:15/AP3A.240905.015.A2/S9160ZHU6DYD9:user/release-keys \
+    BuildDesc="dm2qzhx-user 15 AP3A.240905.015.A2 S9160ZHS6DYF1 release-keys" \
+    BuildFingerprint=samsung/dm2qzhx/dm2q:15/AP3A.240905.015.A2/S9160ZHS6DYF1:user/release-keys \
     DeviceProduct=dm2qzhx \
     SystemName=dm2qzhx
